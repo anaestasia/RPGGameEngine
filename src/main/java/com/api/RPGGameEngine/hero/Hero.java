3e31f08afdddd02d3b74b578e9fa77e3194bc5cd
anaestasia <anaestasia.mathieu@gmail.com>
2026-04-21 11:33:22 +0200

    feat: validation on hero name

--- src/main/java/com/api/RPGGameEngine/hero/Hero.java
+++ src/main/java/com/api/RPGGameEngine/hero/Hero.java
@@ -13,6 +13,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -32,6 +36,12 @@ public class Hero {
     @Column(name = "id", updatable = false, nullable = false)
     private UUID id;
 
+	@NotBlank(message = "Le nom est obligatoire")
+    @Size(min = 2, max = 18, message = "Le nom doit contenir entre 2 et 18 caractères")
+    @Pattern(
+        regexp = "^[a-zA-Z0-9_\\-]+$",
+        message = "Le nom ne peut contenir que des lettres, chiffres, underscores ou tirets"
+    )
     @Column(name = "name", nullable = false, unique = true)
     private String name;
 
@@ -55,10 +65,12 @@ public class Hero {
     @Column(name = "exp", nullable = false)
     private int exp = 0;
 
+    @NotNull(message = "La race est obligatoire")
     @Enumerated(EnumType.STRING)
     @Column(name = "race", nullable = false)
     private HeroRace race;
 
+    @NotNull(message = "Le rôle est obligatoire")
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false)
     private HeroRole role;
